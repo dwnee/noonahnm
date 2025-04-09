ee1168c2d7849f7936fedd6f2fe0a559b17ae46b
Dawon Jung <dwnee0@gmail.com>
2025-04-09 12:21:18 +0900

    first commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src="https://image.hm.com/assets/hm/ac/cc/acccfe3e7c9046d999eea070114cf36162a82538.jpg?imwidth=384"/>
+      <div>Conscious choice</div>
+      <div>벨티드 트월 코트</div>
+      <div>$99990</div>
+      <div>신제품</div>
+    </div>
+  )
+}
+
+export default ProductCard
